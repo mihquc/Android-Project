e26390059ec5e4d7951d155125f98e2068fe04cc
Quốc Nguyễn Minh <102200149@sv1.dut.udn.vn>
2023-03-27 20:18:46 +0700

    mihquccc

--- app/src/main/java/com/example/readingapp/MainActivity.java
+++ app/src/main/java/com/example/readingapp/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.readingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.readingapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
